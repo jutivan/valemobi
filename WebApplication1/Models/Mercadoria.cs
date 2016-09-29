a1296e7e46a2e5e45db9a6e24859958f1e1fdc02
João Roberto Carvalho <jotecarvalho@hotmail.com>
2016-09-29 00:42:30 -0300

    Apos cadastrar ja vai direto para lista

---
+++ WebApplication1/Models/Mercadoria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Valemobi.Models
+{
+    public class Mercadoria
+    {
+        public String nome { get; set; }
+        public int quantidade { get; set; }
+        public double preco { get; set; }
+        public String codigo { get; set; }
+        public String tipo { get; set; }
+        public String tipoNegocio { get; set; }
+
+        public Mercadoria(String nome, String codigo, String tipo, int quantidade, double preco, String tipoNegocio)
+        {
+            this.nome = nome;
+            this.codigo = codigo;
+            this.tipo = tipo;
+            this.quantidade = quantidade;
+            this.preco = preco;
+            this.tipoNegocio = tipoNegocio;
+        }
+    }
+
+
+}
